78cea257a2fe05159a0a41389ec5b57a66a7ca9e
LaiQingFa <2511695066@qq.com>
2017-12-09 23:15:24 +0800

    continue

--- jichu.py
+++ jichu.py
@@ -13,11 +13,22 @@
 #     x = x + 1
 
 #用 for 循环或者 while 循环时，如果要在循环体内直接退出循环，可以使用 break 语句。
-sum = 0
-x = 1
-while True:
+# sum = 0
+# x = 1
+# while True:
+#     sum = sum + x
+#     x = x + 1
+#     if x > 100:
+#         break
+# print(sum)
+
+#在循环过程中，可以用break退出当前循环，还可以用continue跳过后续循环代码，继续下一次循环。
+L = [75, 98, 59, 81, 66, 43, 69, 85]
+sum = 0.0
+n = 0
+for x in L:
+    if x < 60:
+        continue
     sum = sum + x
-    x = x + 1
-    if x > 100:
-        break
-print(sum)
+    n = n + 1
+print(sum / n)
